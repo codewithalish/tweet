3dea77dd7085121649538866982d322e170655dc
codewithalish <95971927+codewithalish@users.noreply.github.com>
2022-01-27 23:52:01 +0330

    add nextCrud package

---
+++ config/crud/base.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'prefix' => 'admin',
+    'middleware' => 'web',
+    'view_namespace' => 'crud::dashboard.',
+    'field_required_span' => "<span style='color: red'>*</span>",
+    'customized_views_path' => 'crud.pages.',
+];
